79b2d471e74d402a7d7e91de2a05e0701cb0f9bd
Sebastian <juan.sorrego98@gmail.com>
2026-08-06 11:42:30 -0500

    feat: add product domain model and repository gateway

---
+++ domain/model/src/main/java/co/com/franchise/model/product/Product.java
@@ -0,0 +1,18 @@
+package co.com.franchise.model.product;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Product {
+    private String id;
+    private String name;
+    private String stock;
+    private String branchId;
+}
